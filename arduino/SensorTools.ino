bff8f2a53c50e8ccd2a1ac4918a89c03d4bac015
Schuyler Ankele <schuyler.ankele@gmail.com>
2016-08-19 01:19:02 -0600

    LEts build some functions

---
+++ arduino/SensorTools.ino
@@ -0,0 +1,38 @@
+float aSensorRead(int pin1, char[2]readPin, int interval){
+    float reading = 0;
+    float sensorReading = 0;
+    float runningTotal;
+    float count = 0;
+    
+    /* Sub-routine for sensor read
+       We will try and have a 10 read minimum as default
+    */
+    if(interval < 10){
+        interval = 10;
+    }
+    
+    /* NOTE: We only power the pin when taking a reading to save power and shut off after for loop */
+    digitalWrite(pin1, HIGH);  
+    delay(500);
+    for(int i=0; i < interval; i++){
+      runningTotal += analogRead(readPin);
+      count++;
+    }
+    /*  Turn off the sensor */
+    digitalWrite(pin1, LOW);
+    
+    /* Print to console */
+    Serial.println("We got ");
+    Serial.println(count);
+    Serial.println("readings");
+    
+    /* Create an average Total_of_All_Readings/Num_Of_Readings */
+    reading = runningTotal/count;
+    
+    /*  We print out our reading to help us test */
+    Serial.println("Sensor Status is:");
+    Serial.println(reading);
+ 
+
+    return reading;
+}
