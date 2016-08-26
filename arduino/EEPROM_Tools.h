5e8c1e40f84dc4ef668bccf82d9a426ed72551d3
Schuyler Ankele <schuyler.ankele@gmail.com>
2016-08-25 23:29:31 -0600

    more functions

--- arduino/EEPROM_Tools.h
+++ arduino/EEPROM_Tools.h
@@ -1,8 +1,17 @@
 #include <EEPROM.h>
+// Our memory map uses 303-720 inclusive for usable memory area (418)
 
-void initBot(){
-    
-    
+/* In this function you can initialize the entire span of memory with on string */
+void initBot(char [418] stringIn){
+    int value = 0;
+    for(int i = 0; i < 418; i++){
+        value = int(stringIn[i]);
+        EEPROM.write(i, value);
+    }    
 }
 
-EEPROM.write(addr, val);
+void readEEPROM(int startingBlock, int chunkSize){
+       for(int i = startingBlock, i < chunkSize, i++;){
+           EEPROM.read
+       }
+}
