72ea4b336bb0728e7003c2a1700ec29f153b956d
Schuyler Ankele <schuyler.ankele@gmail.com>
2016-08-29 00:25:29 -0600

    Arduino_Initilizer

--- arduino/EEPROM_Tool_Usage_Init/EEPROM_Tool_Usage_Init.ino
+++ arduino/EEPROM_Tool_Usage_Init/EEPROM_Tool_Usage_Init.ino
@@ -1,18 +1,24 @@
 #include <EEPROM.h>
 // Our memory map uses 303-720 inclusive for usable memory area (418)
 
+void setup(){
+    Serial.begin(115200);
+    
+    initBot("201608200001011000140000800");
+}
+void loop(){
+  
+}
 /* In this function you can initialize the entire span of memory with on string */
-void initBot(char [418] stringIn){
-    int stop = 720;
+void initBot(char * stringIn){
+    
     int value = 0;
+    int stringSpot = 0;
     for(int address = 303; address < 720; address++){
-        value = int(stringIn[address]);
+        value = stringIn[stringSpot];
         EEPROM.write(address, value);
+        stringSpot++;
     }    
 }
 
-void readEEPROM(int startingBlock, int chunkSize){
-       for(int i = startingBlock, i < chunkSize, i++;){
-           EEPROM.read
-       }
-}
+
