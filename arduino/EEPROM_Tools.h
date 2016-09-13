3dbbdd18e1d21ef4522fb9e7fad724ade310ee9d
Schuyler Ankele <Shoe@Schuylers-Mac-Pro.local>
2016-09-13 01:34:49 -0600

    Still trying to figure this out

--- arduino/EEPROM_Tools.h
+++ arduino/EEPROM_Tools.h
@@ -3,8 +3,20 @@
 //This will count the 100,000 Write to memory allowed
 // 721,722,723,724,725 Maximum of 99,999
 //ex. - 11,322
+int tenBase1 = 0;
+int tenBase2 = 0;
+int tenBase3 = 0;
+int tenBase4 = 0;
+int tenBase5 = 0;
+int tenBase6 = 0;
+
 int RES_CONTING_BLOCK=721
 int counter = 0;    
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
 /* Our memory map uses 303-720 inclusive for usable memory area (418)
   NOTE: on 512k memory we can do 1-418  
 /*
@@ -16,12 +28,40 @@ void initBot(char [418] stringIn){
         value = int(stringIn[address]);
         EEPROM.write(address, value);
     }
-    counter = EEPROM.read(725);
+    //This counter funtion will read and store the amount of times we've written to the EEPROM
     
-    counter = counter + (EEPROM.read(726)*10);
-    counter = counter + (EEPROM.read(727)*100);
-    counter = counter + (EEPROM.read(728)*1000);
-    counter = counter + (EEPROM.read(729)*10000);
+    tenBase1 = EEPROM.read(729);
+    if(tenBase1 == 9){
+        tenBase1=0;
+        counter1 = 10;
+    }
+    else{
+        tenBase1++;
+    }
+    tenBase2 = EEPROM.read(728)*10;
+    if(tenBase2==9 && counter1==10){
+        tenBase2==0;
+        counter2=100;
+        
+    }
+    tenBase3 = EEPROM.read(727)*100;
+    if(tenBase3 ==9 && counter2==100){
+        tenBase3=0;
+        counter3=1000;
+    }
+    tenBase4 = EEPROM.read(726)*1000;
+    if(tenBase4 ==9 && counter3==1000){
+        tenBase4=0;
+        counter4=10000;
+    }
+    tenBase5 = EEPROM.read(725)*10000;
+    if(tenBase5 ==9 && counter4==10000){
+        tenBase5=0;
+        counter5=100000;
+    }
+    counter = tenBase1 + tenBase2 + tenBase3 + tenBase4 + tenBase5 + counter1 + counter2 + counter3 + counter4 + counter5;
+    Serial.println(counter);
+    if()
    
     
 }
