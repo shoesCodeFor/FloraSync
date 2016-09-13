973036891e914dcb0f6b73cee36ba8419f01a9ad
Schuyler Ankele <Shoe@Schuylers-Mac-Pro.local>
2016-09-13 02:18:29 -0600

    Holy Moly more memory protection
    Arduino memory is fussy.  I think all EEPROM is

--- arduino/EEPROM_Tools.h
+++ arduino/EEPROM_Tools.h
@@ -3,24 +3,17 @@
 //This will count the 100,000 Write to memory allowed
 // 721,722,723,724,725 Maximum of 99,999
 //ex. - 11,322
-int tenBase1 = 0;
-int tenBase2 = 0;
-int tenBase3 = 0;
-int tenBase4 = 0;
-int tenBase5 = 0;
-int tenBase6 = 0;
+int tenBase1, tenBase2, tenBase3, tenBase4, tenBase5 = 0;
+
 
 int RES_CONTING_BLOCK=721
 int counter = 0;    
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
+int counter1, counter2, counter3, counter4, counter5 = 0;
+
 /* Our memory map uses 303-720 inclusive for usable memory area (418)
   NOTE: on 512k memory we can do 1-418  
 /*
-/* In this function you can initialize the entire span of memory with on string */
+/* In this function you can initialize the entire span of memory with one string */
 void initBot(char [418] stringIn){
     int stop = 720;
     int value = 0;
@@ -28,43 +21,12 @@ void initBot(char [418] stringIn){
         value = int(stringIn[address]);
         EEPROM.write(address, value);
     }
-    //This counter funtion will read and store the amount of times we've written to the EEPROM
     
-    tenBase1 = EEPROM.read(729);
-    if(tenBase1 == 9){
-        tenBase1=0;
-        counter1 = 10;
-    }
-    else{
-        tenBase1++;
-    }
-    tenBase2 = EEPROM.read(728)*10;
-    if(tenBase2==9 && counter1==10){
-        tenBase2==0;
-        counter2=100;
-        
-    }
-    tenBase3 = EEPROM.read(727)*100;
-    if(tenBase3 ==9 && counter2==100){
-        tenBase3=0;
-        counter3=1000;
-    }
-    tenBase4 = EEPROM.read(726)*1000;
-    if(tenBase4 ==9 && counter3==1000){
-        tenBase4=0;
-        counter4=10000;
-    }
-    tenBase5 = EEPROM.read(725)*10000;
-    if(tenBase5 ==9 && counter4==10000){
-        tenBase5=0;
-        counter5=100000;
-    }
-    counter = tenBase1 + tenBase2 + tenBase3 + tenBase4 + tenBase5 + counter1 + counter2 + counter3 + counter4 + counter5;
-    Serial.println(counter);
-    if()
-   
+   memWriteProtect();
     
 }
+
+
 
 
 // This will return an entire char array
@@ -87,4 +49,84 @@ char readEEPROM(int memoryBlock){
         char oneChar;
         oneChar = EEPROM.read(memoryBlock);
         return oneChar;    
+}
+
+void memWriteProtect(){
+    //This counter funtion will read and store the amount of times we've written to the EEPROM
+    
+    
+    // Ten Base 1 = 1-9
+    tenBase1 = EEPROM.read(729);
+    if(tenBase1 == 9){
+        tenBase1=0;
+        counter1 = 10;
+    }
+    else{
+        tenBase1++;
+    }
+    
+    // Ten Base 2 = 10-90
+    tenBase2 = EEPROM.read(728);//10
+    if(tenBase2==9 && counter1==10){
+        tenBase2==0;
+        counter2=100;
+    }
+    else if(counter1==10){
+        tenBase2++;
+    }
+    
+    // Ten Base 3 = 100-900
+    tenBase3 = EEPROM.read(727);//100
+    if(tenBase3 ==9 && counter2==100){
+        tenBase3=0;
+        counter3=1000;
+    }
+    else if(counter2==100){
+        tenBase3++;
+    }
+    
+    // Ten Base 4 = 1000-9000
+    tenBase4 = EEPROM.read(726);//1000
+    if(tenBase4 ==9 && counter3==1000){
+        tenBase4=0;
+        counter4=10000;
+    }
+    else if(counter3==1000){
+        tenBase4++;
+    }
+    
+    // Ten Base 5 = 10000-90000
+    tenBase5 = EEPROM.read(725);//10000
+    if(tenBase5 ==9 && counter4==10000){
+        tenBase5=0;
+        counter5=100000;
+    }
+    else if(counter4==10000){
+        tenBase5++;
+        
+    }
+    
+    counter = tenBase1 + (tenBase2*10) + (tenBase3*100) + (tenBase4*1000) + (tenBase5*10000);
+    if(counter5==100000){
+        counter=99999;// We're maxed out
+        Serial.println("DANGER: MEMORY LOSS RISK DETECTED.  You can only safely write 100,000 time to EEPROM");
+    }
+    if(counter==99999){
+        EEPROM.write(729, 9);
+        EEPROM.write(728, 9);
+        EEPROM.write(727, 9);
+        EEPROM.write(726, 9);
+        EEPROM.write(725, 9);
+    }
+    else{
+        EEPROM.write(729, tenBase1);
+        EEPROM.write(728, tenBase2);
+        EEPROM.write(727, tenBase3);
+        EEPROM.write(726, tenBase4);
+        EEPROM.write(725, tenBase5);
+    }
+    Serial.println("Written to memory");
+    Serial.println(counter);
+    Serial.println("times");
+    
 }
